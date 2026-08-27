35198063475dd8abd5b8cc98716462cf882f0ea0
sandhiyasenthil2397-code <sandhiyasenthil2397@gmail.com>
2026-08-27 11:13:25 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,3 +1,16 @@
+USE CollegeDB;
+
+CREATE TABLE Student (
+    StudentID INT(5) PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT(5) NOT NULL,
+    CONSTRAINT UQ_StudentName UNIQUE (StudentName),
+    CONSTRAINT FK_Student_Department
+        FOREIGN KEY (DepartmentID)
+        REFERENCES Department(DepartmentID)
+);
 DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
